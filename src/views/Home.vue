9dadcdd9accb87dc186c837db2b8367768a63f54
Your Name <you@example.com>
2020-12-18 11:31:28 +0800

    write your meaaage

---
+++ src/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="home-container">
+        <el-switch
+            v-model="value"
+            active-color="#13ce66"
+            inactive-color="#ff4949">
+        </el-switch>
+        <div class="home-content">
+            <Button @click="getUserData">ajax 测试</Button>
+
+            <Input :rows="30" style="margin-top: 20px" v-model="userInfo" type="textarea" />
+        </div>
+
+    </div>
+</template>
+
+<script>
+import { fetchUserData } from '@/api'
+
+export default {
+    name: 'home',
+    data() {
+        return {
+            userInfo: '',
+            value: true,
+        }
+    },
+    methods: {
+        getUserData() {
+            fetchUserData().then(res => {
+                this.userInfo = JSON.stringify(res, null, 4)
+            })
+        },
+    },
+}
+</script>
+
+<style scoped>
+.home-container {
+    padding: 10px;
+    padding-top: 5px;
+}
+.home-content {
+    padding: 10px;
+    border-radius: 5px;
+    background: #fff;
+}
+</style>
